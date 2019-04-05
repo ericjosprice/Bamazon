0aa7557aa6289011edc33ea5cce3544d1f463e02
ericjosprice <epriceutexas@gmail.com>
2019-04-05 17:41:20 -0500

    added schema file

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products(
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  price DEC default 0,
+  stock_quantity INT default 0,
+  PRIMARY KEY (item_id)
+);
